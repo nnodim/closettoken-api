9a208d809b51b908bdf7581b2f375dd4033ba9e3
nnodim <nnodimobinna@gmail.com>
2023-08-22 07:54:12 +0100

    addded vercel.json

--- index.js
+++ index.js
@@ -48,3 +48,5 @@ mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
+
+module.exports = app;
